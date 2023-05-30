238c8f5280000dc4a1e3ad1dee0e84dea759ff30
a.lazev@transdata.no <a.lazev@transdata.no>
2023-05-30 12:26:23 +0300

    path('tasks/delete/<int:task_id>/', delete_task, name='DeleteTask')

--- TasksAPP/urls.py
+++ TasksAPP/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from TasksAPP.views import index, create_task
+from TasksAPP.views import index, create_task, delete_task
 
 urlpatterns = [
     path('', index, name='HomePage'),
-    path('create/', create_task, name='CreateTask')
+    path('create/', create_task, name='CreateTask'),
+    path('tasks/delete/<int:task_id>/', delete_task, name='DeleteTask')
 ]
