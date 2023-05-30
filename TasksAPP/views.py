ae7449a324232e5397d42aa92a677fb7b7001881
a.lazev@transdata.no <a.lazev@transdata.no>
2023-05-30 12:28:58 +0300

    PEP8

--- TasksAPP/views.py
+++ TasksAPP/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from TasksAPP.forms import CreateTaskForm
 from TasksAPP.models import Tasks
 
